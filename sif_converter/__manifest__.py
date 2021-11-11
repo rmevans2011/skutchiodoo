4db7e179bcd042f3e87d70bb108a4f6326c3917e
Robert Evans <robert@skutchi.com>
2021-11-11 19:25:12 +0000

    Reenable Security

--- sif_converter/__manifest__.py
+++ sif_converter/__manifest__.py
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        #'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/import_job.xml'        
     ],
     # only loaded in demonstration mode
