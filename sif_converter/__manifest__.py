cd3da59675e481a9e4e274e70f10375f7abc5ee3
Robert Evans <Robert Evans>
2021-11-11 16:16:20 +0000

    Initial Module Commit

---
+++ sif_converter/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Sif Converter',
+    'version' : '0.1',
+    'summary': 'Sif Converter',
+    'sequence': 10,
+    'description': """Module to import sif files into estimates""",
+    'category': 'Accounting/Accounting',
+    'website': 'https://www.odoo.com/app/billing',
+    'depends' : ['sale_management'],
+    'data': [],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
