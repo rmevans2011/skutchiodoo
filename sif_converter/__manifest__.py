5c4cef36ced8cabf6eb782ba37252981db5c68d3
Robert Evans <robert@skutchi.com>
2021-11-11 20:22:21 +0000

    Import view file in manifest

--- sif_converter/__manifest__.py
+++ sif_converter/__manifest__.py
@@ -25,7 +25,8 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/import_job.xml'        
+        'views/import_job.xml',
+        'views/sale.xml'
     ],
     # only loaded in demonstration mode
     'demo': [],
