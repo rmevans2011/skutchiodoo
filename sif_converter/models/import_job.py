c1e17a4043509aad48b95b5d9d14fe6122ee4806
Robert Evans <robert@skutchi.com>
2021-11-12 10:47:28 -0500

    Add field tracking

--- sif_converter/models/import_job.py
+++ sif_converter/models/import_job.py
@@ -7,13 +7,13 @@ class import_job(models.Model):
     _inherit = ["mail.thread", 'mail.activity.mixin']
     _description = "Import Job"
 
-    customer_name = fields.Char(string='Customer Name', required=True, translate=True)
-    short_description = fields.Char(string="Short Description")
+    customer_name = fields.Char(string='Customer Name', required=True, translate=True, tracking=True)
+    short_description = fields.Char(string="Short Description", tracking=True)
     state = fields.Selection([
         ('new_import', 'New Import Job'), ('needs_matching', 'Needs Matching'),
         ('estimate_ready', 'Ready to Create Estimate'), ('done', 'Done'),
         ('cancel', 'Cancelled')
-    ], default="new_import", string="Status")
+    ], default="new_import", string="Status", tracking=True)
 
     def action_confirm(self):
         self.state = 'needs_matching'
