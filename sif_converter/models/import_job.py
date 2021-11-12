9afbd47809a4277ee1744bb849fada380b96737d
Robert Evans <robert@skutchi.com>
2021-11-12 09:41:51 -0500

    Add activities to chatter

--- sif_converter/models/import_job.py
+++ sif_converter/models/import_job.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models
 
 class import_job(models.Model):
     _name = "sif_converter.import_job"
-    _inherit = ["mail.thread"]
+    _inherit = ["mail.thread", 'mail.activity.mixin']
     _description = "Import Job"
 
     customer_name = fields.Char(string='Customer Name', required=True, translate=True)
