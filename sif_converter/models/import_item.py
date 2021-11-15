10328a15a0279e33b6a6e45d2be7c486eb883522
Robert Evans <robert@skutchi.com>
2021-11-15 14:59:56 -0500

    Fix tracking error

--- sif_converter/models/import_item.py
+++ sif_converter/models/import_item.py
@@ -8,5 +8,5 @@ class import_item(models.Model):
     _description = "Import Job Import Items"
 
     import_job_id = fields.Many2one('sif_converter.import_job', string='Import Job')
-    sif_sku = fields.Char(string='Sif Sku', required=True, translate=True, tracking=True)
-    sif_options = fields.Char(string='Sif Options', required=True, tracking=True)
+    sif_sku = fields.Char(string='Sif Sku', required=True, translate=True)
+    sif_options = fields.Char(string='Sif Options', required=True)
