f6cff105a94427870b92ac76eec975493cb965de
Robert Evans <robert@skutchi.com>
2021-11-15 16:26:54 -0500

    Add generic code to model

--- sif_converter/models/import_item.py
+++ sif_converter/models/import_item.py
@@ -10,4 +10,5 @@ class import_item(models.Model):
     import_job_id = fields.Many2one('sif_converter.import_job', string='Import Job')
     sif_sku = fields.Char(string='Sif Sku', required=True, translate=True)
     sif_options = fields.Char(string='Sif Options', required=True)
+    generic_code = fields.Char(string='Generic Code', required=True)
     needs_matching = fields.Boolean(string='Needs to be matched')
