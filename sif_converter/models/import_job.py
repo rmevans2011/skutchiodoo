59d20a27ef28dbec7c817b6369537912f463ee8b
Robert Evans <robert@skutchi.com>
2021-11-19 14:27:47 -0500

    Remove last character from sif_converter

--- sif_converter/models/import_job.py
+++ sif_converter/models/import_job.py
@@ -66,7 +66,7 @@ class import_job(models.Model):
             import_row_vals = {
                 'import_job_id': import_job_id,
                 'sif_sku': row[2],
-                'sif_options': row[4].replace('\xa0', '|'),
+                'sif_options': row[4].replace('\xa0', '|')[:-1],
                 'qty': row[1],
                 'generic_code': row[32],
                 'needs_matching': False
