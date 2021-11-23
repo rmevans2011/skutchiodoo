b9159c6bc455e209d8401af37d9b4e5586b0ae77
Robert Evans <robert@skutchi.com>
2021-11-23 14:55:45 -0500

    Fix variant sku

--- sif_converter/models/product_template.py
+++ sif_converter/models/product_template.py
@@ -8,10 +8,10 @@ from odoo.osv import expression
 
 _logger = logging.getLogger(__name__)
 
-class product_template(models.Model):
+class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    variant_sku = fields.Char(string="Variant Sku", compute='_compute_variant_sku')
+    variant_sku = fields.Char(string="Variant Sku")
 
     def _compute_variant_sku(self):
         self.variant_sku = self.default_code
@@ -105,3 +105,34 @@ class product_template(models.Model):
         self.flush()
         self.invalidate_cache()
         return True
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        ''' Store the initial standard price in order to be able to retrieve the cost of a product template for a given date'''
+        for vals in vals_list:
+            self._sanitize_vals(vals)
+        templates = super(ProductTemplate, self).create(vals_list)
+        if "create_product_product" not in self._context:
+            templates._create_variant_ids()
+
+        # This is needed to set given values to first variant after creation
+        for template, vals in zip(templates, vals_list):
+            related_vals = {}
+            if vals.get('barcode'):
+                related_vals['barcode'] = vals['barcode']
+            if vals.get('default_code'):
+                related_vals['default_code'] = vals['default_code']
+                related_vals['variant_sku'] = vals['default_code']
+            if vals.get('standard_price'):
+                related_vals['standard_price'] = vals['standard_price']
+            if vals.get('volume'):
+                related_vals['volume'] = vals['volume']
+            if vals.get('weight'):
+                related_vals['weight'] = vals['weight']
+            # Please do forward port
+            if vals.get('packaging_ids'):
+                related_vals['packaging_ids'] = vals['packaging_ids']
+            if related_vals:
+                template.write(related_vals)
+
+        return templates
