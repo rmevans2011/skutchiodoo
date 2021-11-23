c0e058a2fa39d3be92c32d2d302224eb639b1d26
Robert Evans <robert@skutchi.com>
2021-11-23 11:56:54 -0500

    Add custom order field to product_attribute

--- sif_converter/models/product_attribute.py
+++ sif_converter/models/product_attribute.py
@@ -6,3 +6,4 @@ class product_attribute(models.Model):
     _inherit = "product.attribute"
 
     friendly_name = fields.Char(string="Attribute Display Name")
+    custom_order = fields.Integer(string="Custom Order")
