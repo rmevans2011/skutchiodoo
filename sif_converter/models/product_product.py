b2d1fcc4dc1dc9d23facabb84e90a5648085afe8
Robert Evans <robert@skutchi.com>
2021-11-23 14:05:23 -0500

    Fix insert

--- sif_converter/models/product_product.py
+++ sif_converter/models/product_product.py
@@ -20,7 +20,7 @@ class ProductProduct(models.Model):
                 variant_description = ""
                 variant_sku_parts = []
                 for i in range(len(prod.attribute_line_ids)):
-                    variant_sku_parts.append(0, "-" + prod.product_template_attribute_value_ids[i].product_attribute_value_id.name.split(' ')[0])
+                    variant_sku_parts.insert(0, "-" + prod.product_template_attribute_value_ids[i].product_attribute_value_id.name.split(' ')[0])
                 _logger.info("Variant SKU: " + variant_sku.join(variant_sku_parts))
         # `_get_variant_id_for_combination` depends on existing variants
         self.clear_caches()
