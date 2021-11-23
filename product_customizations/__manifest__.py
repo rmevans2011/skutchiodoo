5f399f39e26c0cb7b60d2b2b9e09649fb45830da
Robert Evans <robert@skutchi.com>
2021-11-23 16:15:51 -0500

    Initial Schedule

---
+++ product_customizations/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "product_customizations",
+
+    'summary': """
+        Module to add customizations to products and their attributes""",
+
+    'description': """
+        Module to add customizations to products and their attributes
+    """,
+
+    'author': "Skutchi Designs",
+    'website': "http://www.skutchi.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'license': 'LGPL-3',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base',
+                'sale',
+                'mail'],
+
+    # always loaded
+    'data': [
+        'views/product_attribute.xml'
+    ],
+    # only loaded in demonstration mode
+    'demo': [],
+    'application': True,
+    'installable': True
+}
