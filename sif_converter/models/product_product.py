11abcbe8f7c70287b8b40247c81853951fd26eb8
Robert Evans <robert@skutchi.com>
2021-11-23 09:59:35 -0500

    Override product.product create method

--- sif_converter/models/product_product.py
+++ sif_converter/models/product_product.py
@@ -1,5 +1,17 @@
 from odoo import api, fields, models
+import logging
 
+_logger = logging.getLogger(__name__)
 
-class product_product(models.Model):
+class ProductProduct(models.Model):
     _inherit = "product.product"
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        _logger.info("Override Create Method for Products")
+        for vals in vals_list:
+            self.product_tmpl_id._sanitize_vals(vals)
+        products = super(ProductProduct, self.with_context(create_product_product=True)).create(vals_list)
+        # `_get_variant_id_for_combination` depends on existing variants
+        self.clear_caches()
+        return products
