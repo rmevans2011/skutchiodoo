45aa19e5370dfdcd1bd2f0c34cbf5d921b25c966
Robert Evans <robert@skutchi.com>
2021-11-24 15:33:37 -0500

    Initial Commit

---
+++ sif_converter/models/import_job.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+import logging
+import base64
+import csv
+import io
+_logger = logging.getLogger(__name__)
+
+
+class import_job(models.Model):
+    _name = "sif_converter.import_job"
+    _inherit = ["mail.thread", 'mail.activity.mixin']
+    _description = "Import Job"
+
+    customer_id = fields.Many2one('res.partner', string='Customer', required=True, tracking=True)
+    customer_name = fields.Char(string='Customer Name', required=True, translate=True, tracking=True)
+    short_description = fields.Char(string="Short Description", tracking=True)
+    csv_file = fields.Binary(string="CSV File")
+    state = fields.Selection([
+        ('new_import', 'New Import Job'), ('needs_matching', 'Needs Matching'),
+        ('estimate_ready', 'Ready to Create Estimate'), ('done', 'Done'),
+        ('cancel', 'Cancelled')
+    ], default="new_import", string="Status", tracking=True)
+    import_item_ids = fields.One2many('import_job.import_item.lines', 'import_job_id', string="Imported Items")
+    nm_import_item_ids = fields.One2many('import_job.import_item.lines', 'import_job_id',
+                                         string="Needs Matching Imported Items", compute='_compute_nm_import_item_ids')
+
+    def _compute_nm_import_item_ids(self):
+        needs_matching_items = self.env['import_job.import_item.lines'].search([('import_job_id', '=', self.id),
+                                                                                ('needs_matching', '=', True)])
+        self.nm_import_item_ids = needs_matching_items
+
+    def action_confirm(self):
+        order_vals = {
+            'partner_id': self.customer_id.id
+        }
+        order = self.env['sale.order'].create(order_vals)
+        line_items = self.import_item_ids
+        for line_item in line_items:
+            item_vals = {
+                'order_id': order.id,
+                'product_uom_qty': line_item.qty,
+                'product_id': line_item.product_id.id
+            }
+            self.env['sale.order.line'].create(item_vals)
+        self.state = 'needs_matching'
+
+    def action_estimate(self):
+        self.state = 'estimate_ready'
+
+    def action_reset(self):
+        self.state = 'new_import'
+
+    def action_cancel(self):
+        self.state = 'cancel'
+
+    @api.model
+    def create(self, vals):
+        # Create Import Job
+        new_status = "estimate_ready"
+        res = super(import_job, self).create(vals)
+        _logger.info(res)
+        _logger.info(res.id)
+        import_job_id = res.id
+        Product = self.env['product.product']
+        Matched_Product = self.env['sif_converter.matched_product']
+
+        #Process csv file
+        decoded_csv_file = base64.b64decode(vals['csv_file'])
+        data = io.StringIO(decoded_csv_file.decode("unicode_escape"))
+        csv_reader = csv.reader(data)
+        next(csv_reader)
+        for row in csv_reader:
+            next_code = False
+            search_sku = row[2]
+            _logger.info(row)
+            _logger.info(row[4].split())
+            _logger.info(row[5].split('|'))
+
+            options = row[4].replace('\xa0', '|').split('|')
+            if(len(options) > 0):
+                _logger.info("Need To Build SKU")
+                ent = row[36]
+                pline = row[0]
+                if(ent == 'SKU'):
+                    if(pline == 'ECS'):
+                        for opt in options:
+                            if(next_code):
+                                next_code = False
+                                search_sku += '-'+opt
+                            else:
+                                if(opt == 'FAB'):
+                                    next_code = True
+                                if (opt == 'PET'):
+                                    next_code = True
+                                if (opt == 'WB'):
+                                    search_sku += '-WB'
+                _logger.info("Completed Search SKU: " + search_sku)
+            else:
+                _logger.info("No Need To Build SKU")
+
+            import_row_vals = {
+                'import_job_id': import_job_id,
+                'sif_sku': row[2],
+                'sif_options': row[4].replace('\xa0', '|')[:-1],
+                'search_sku': search_sku,
+                'qty': row[1],
+                'generic_code': row[32],
+                'needs_matching': False
+            }
+
+            p_search = Product.search([('default_code', '=', search_sku)])
+            if(len(p_search) == 0):
+                # Continue needs matching logic
+                mp_search = Matched_Product.search([('sif_sku', '=', row[2]),
+                                                   ('sif_options', '=', row[4].replace('\xa0', '|'))])
+                if(len(mp_search) == 0):
+                    import_row_vals['needs_matching'] = True
+                    new_status = "needs_matching"
+                else:
+                    _logger.info("Matched matched_product")
+                    import_row_vals['product_id'] = mp_search.product_id.id
+                    import_row_vals['matched_product_id'] = mp_search.id
+            else:
+                _logger.info("Matched regular product")
+                _logger.info(p_search.id)
+                import_row_vals['product_id'] = p_search.id
+
+            self.env['import_job.import_item.lines'].create(import_row_vals)
+            _logger.info(len(self.env['product.product'].search([('default_code', '=', 'E-COM111')])))
+
+        res.state = new_status
+        if not vals.get('short_description'):
+            vals['short_description'] = "Some text"
+        return res
---
+++ sif_converter/models/matched_product.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class matched_product(models.Model):
+    _name = "sif_converter.matched_product"
+    _inherit = ["mail.thread", 'mail.activity.mixin']
+    _description = "Matched Products"
+
+    sif_sku = fields.Char(string="Sif Sku")
+    sif_options = fields.Char(string="Sif Options")
+    product_id = fields.Many2one('product.product', string="Matched Product")
