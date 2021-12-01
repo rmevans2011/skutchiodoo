b7f4775bd9233d7786833ce2d1d4e897aebad6f3
Robert Evans <robert@skutchi.com>
2021-12-01 11:24:47 -0500

    Update product_product model

--- product_customizations/models/product_product.py
+++ product_customizations/models/product_product.py
@@ -7,6 +7,7 @@ class ProductProduct(models.Model):
     _inherit = "product.product"
 
     variant_description = fields.Text(string="Variant Desc")
+    computed_description = fields.Text(string="Base Product Description")
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -23,7 +24,7 @@ class ProductProduct(models.Model):
                     variant_description = ""
                     variant_sku_parts = []
                     for i in range(len(prod.attribute_line_ids)):
-                        variant_description += "\n"
+                        variant_description += "\n\t-"
                         variant_description += prod.attribute_line_ids[i].attribute_id.name + ": " + prod.product_template_attribute_value_ids[
                             i].product_attribute_value_id.name
                         variant_sku_parts.insert(0, "-" + prod.product_template_attribute_value_ids[i].product_attribute_value_id.name.split(' ')[0])
@@ -31,6 +32,9 @@ class ProductProduct(models.Model):
                     _logger.info("Variant SKU: " + variant_sku+end_sku)
                     prod.default_code = variant_sku+end_sku
                     prod.variant_description = variant_description
+                    prod.computed_description = prod.description_sale+prod.variant_description
+                else:
+                    prod.computed_description = prod.description_sale
         # `_get_variant_id_for_combination` depends on existing variants
         self.clear_caches()
         return products
