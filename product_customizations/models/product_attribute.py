e601f890c5f621a85d3544611e2922613cd32c10
Robert Evans <robert@skutchi.com>
2021-12-01 13:28:35 -0500

    Add dispaly name to product_attribute

--- product_customizations/models/product_attribute.py
+++ product_customizations/models/product_attribute.py
@@ -6,4 +6,5 @@ class product_attribute(models.Model):
     _inherit = "product.attribute"
     _order = "custom_order asc"
 
+    display_name = fields.Char(string="Product Description")
     custom_order = fields.Integer(string="Custom Order")
