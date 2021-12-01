ee227156630cd964bb32a0fe2b6f4ee28dcfdcab
Robert Evans <robert@skutchi.com>
2021-12-01 12:05:21 -0500

    Clean up product_attribute

--- product_customizations/models/product_attribute.py
+++ product_customizations/models/product_attribute.py
@@ -6,5 +6,4 @@ class product_attribute(models.Model):
     _inherit = "product.attribute"
     _order = "custom_order asc"
 
-    att_sku = fields.Char(string="Attribute Sku")
     custom_order = fields.Integer(string="Custom Order")
