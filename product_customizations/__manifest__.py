244fc4c93f998341ad7e90b19139591fc6101f20
Robert Evans <robert@skutchi.com>
2021-12-03 12:23:11 -0500

    Update project version

--- product_customizations/__manifest__.py
+++ product_customizations/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '1.5',
+    'version': '1.5.3',
     'license': 'LGPL-3',
 
     # any module necessary for this one to work correctly
