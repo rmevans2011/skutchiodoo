157a43c66ea6fae82197603f17e5c298a5376cb6
Robert Evans <robert@skutchi.com>
2021-12-03 15:33:10 -0500

    Remove product models

--- sif_converter/models/__init__.py
+++ sif_converter/models/__init__.py
@@ -2,6 +2,3 @@ from . import import_job
 from . import import_item
 from . import sale
 from . import matched_product
-from . import product_product
-from . import product_attribute
-from . import product_template
