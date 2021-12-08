314b87a52d9697c03c1d6e690869abef17332394
Robert Evans <robert@skutchi.com>
2021-12-08 13:23:01 -0500

    Fix addition

--- sif_converter/models/import_job.py
+++ sif_converter/models/import_job.py
@@ -41,7 +41,7 @@ class import_job(models.Model):
         line_items = self.import_item_ids
         for line_item in line_items:
             if(str(line_item.product_id.id)+"p" in sale_order_lines):
-                sale_order_lines[str(line_item.product_id.id)+"p"]['qty'] += line_item.qty
+                sale_order_lines.get(str(line_item.product_id.id)+"p")['qty'] += line_item.qty
             else:
                 sale_order_lines[str(line_item.product_id.id)+"p"] = {
                     'prod_id': line_item.product_id.id,
