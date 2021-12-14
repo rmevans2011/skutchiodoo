b9346a2e75c6782e67dd8926db5adc31d2567dae
Robert Evans <robert@skutchi.com>
2021-12-14 15:00:24 -0500

    Add display description

--- sif_converter/models/sale_order_line.py
+++ sif_converter/models/sale_order_line.py
@@ -6,3 +6,4 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     custom_notes = fields.Text(string="Custom Notes")
+    display_description = fields.Text(string="Display Description")
