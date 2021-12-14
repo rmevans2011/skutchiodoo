688f904d5740f6bf95eb7bb179cb0d87be5d7462
Robert Evans <robert@skutchi.com>
2021-12-14 15:05:04 -0500

    Write compute function

--- sif_converter/models/sale_order_line.py
+++ sif_converter/models/sale_order_line.py
@@ -7,3 +7,7 @@ class SaleOrderLine(models.Model):
 
     custom_notes = fields.Text(string="Custom Notes")
     display_description = fields.Text(string="Display Description")
+
+    def _compute_display_description(self):
+        for rec in self:
+            rec.display_description = rec.product_id.computed_description
