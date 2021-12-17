79461f79a11281d99600bbc7c7c8c71cf4b6dfdf
Robert Evans <robert@skutchi.com>
2021-12-17 16:37:11 -0500

    Save merged lines to database

--- sif_converter/models/__init__.py
+++ sif_converter/models/__init__.py
@@ -5,3 +5,4 @@ from . import matched_product
 from . import sif_sku
 from . import sale_order_line
 from . import product_category
+from . import merged_lines
---
+++ sif_converter/models/merged_lines.py
@@ -0,0 +1,15 @@
+from odoo import api, fields, models
+import logging
+_logger = logging.getLogger(__name__)
+
+class merged_lines(models.Model):
+    _name = "import_job.merged_lines"
+    _description = "Merged Lines"
+
+    import_job_id = fields.Many2one('sif_converter.import_job', string='Import Job')
+    prod_id = fields.Many2one('product.product', string='Matched Product')
+    custom_notes = fields.Text(string="Custom Notes 3")
+    price_unit = fields.Float(string="Product Upcharge")
+    category_code = fields.Char(string="Category Code")
+    categ_header = fields.Char(string="Category Header")
+    qty = fields.Integer(string='Quantity')
