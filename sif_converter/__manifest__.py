f4d18741b9781237f93413625bf7524439f621d3
Robert Evans <robert@skutchi.com>
2021-12-17 16:05:03 -0500

    Add sort functionality to categories

--- sif_converter/__manifest__.py
+++ sif_converter/__manifest__.py
@@ -30,7 +30,8 @@
         'views/import_job.xml',
         'views/matched_product.xml',
         'views/sif_sku.xml',
-        'views/sale.xml'
+        'views/sale.xml',
+        'views/product_category.xml'
     ],
     # only loaded in demonstration mode
     'demo': [],
